607573d618e33c50beeef2f3f25fdc1d6ef1d748
Heru <tantoyk7@gmail.com>
2025-02-13 16:22:42 +0700

    learn: link & navigation in nextjs

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+const LoginPage = () => {
+    const {push} = useRouter()
+    const handlerLogin = () => {
+        push('/product')
+    }
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <button onClick={()=>handlerLogin()}>Login</button>
+            <p>Belum punya akun? Register <Link href={'/auth/register'}>disini</Link></p>
+        </div>
+    )
+}
+export default LoginPage
